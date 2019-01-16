1fe791b2399bb319548ee8c9632158cf53842fef
ohmycloud <ohmycloudy@gmail.com>
2019-01-16 13:56:01 +0800

    fix the eventEndTime bug

--- multi-events/src/main/scala/com/gac/xs6/bigdata/core/impl/EventsCheckImpl.scala
+++ multi-events/src/main/scala/com/gac/xs6/bigdata/core/impl/EventsCheckImpl.scala
@@ -58,7 +58,7 @@ object EventsCheckImpl extends EventsCheck with Logging {
       "alm_common_esd_charge_over"
     )
 
-    val sinkUpdate: mutable.HashMap[String, EventUpdate] = scala.collection.mutable.HashMap() // 全局的存放输出结果
+    // val sinkUpdate: mutable.HashMap[String, EventUpdate] = scala.collection.mutable.HashMap() // 全局的存放输出结果
 
     if (state.isTimingOut() && state.exists()) {
       val lastState = state.get()
@@ -67,85 +67,87 @@ object EventsCheckImpl extends EventsCheck with Logging {
       Some((vin, lastState))                         // 发送到 downstream, 但不移除状态
     } else opt.flatMap( data => {
 
-      val dataEvents = data.eventMaps            // data 中的 eventMaps
-      val newTime    = data.ts                   // 新的数据的时间戳
-      val lastState  = getLastState(data, state) // 获取每个事件上一次的状态, state 中的 eventMaps, 只获取
-      val cc = lastState
-      val dd = state
+      val dataEvents = data.eventMaps             // data 中的 eventMaps
+      val newTime    = data.ts                    // 新的数据的时间戳
+      val lastEvents  = getLastEvent(data, state) // 获取每个事件上一次的状态
 
-      eventsList.map { evName =>
+      eventsList.map { topicEvent =>
 
-        if (dataEvents.contains(evName) &&
-             lastState.contains(evName)
+        if (dataEvents.contains(topicEvent) &&
+            lastEvents.contains(topicEvent)
         ) {
           // 判断事件结束，初始化或更新
-          lastState.map { ev =>
-            // 对于每一个事件, 用 newTime - eventEndTime
-            val evName   = ev._1
-            val evUpdate = ev._2
-            if ( (newTime - evUpdate.eventEndTime) >= eventDuration.milliseconds ) { // 事件正常结束
-              println("开始划分事件了： 事件名, 事件开始时间, 事件结束时间, 事件持续时长", evName, evUpdate.eventStartTime, evUpdate.eventEndTime, evUpdate.eventDuration)
-              evUpdate.eventStatus  = 0        // 这种类型的事件是正常结束的事件, 将事件类型置为 0, 更新了 lastState
-              evUpdate.eventEndTime = newTime  // 把新的源数据的时间戳作为上一个事件的事件结束时间, 更新了 lastState
+          lastEvents.map { ev =>
+            val eventName   = ev._1
+            val eventUpdate = ev._2
 
-              initState(data, evName, state)  // 初始化新的事件
+            if ( (newTime - eventUpdate.eventEndTime) >= eventDuration.milliseconds ) { // 事件正常结束
+
+              eventUpdate.eventStatus = 0  // 将事件类型置为 0, 即结束上一段事件
+
+              val eventStartTime = eventUpdate.eventStartTime
+              val eventEndTime   = eventUpdate.eventEndTime
+              val eventDuration  = eventUpdate.eventDuration
+
+              println(f"划分了一个事件: " +
+                f"事件名: $eventName, " +
+                f"事件开始时间: $eventStartTime, " +
+                f"事件结束时间：, $eventEndTime" +
+                f"事件持续时长:  $eventDuration"
+              )
+
+              initState(data, eventName, state)  // 初始化新的事件
 
             } else { // 事件正在进行
-              val newState = getUpdateState(data, evName, lastState)
-              state.update(newState) // 用 newState 更新旧的状态
+              val updatedState = getUpdatedEvent(data, eventName, lastEvents)
+              state.update(updatedState) // 用 newState 更新旧的状态
               None  // 在内存中更新事件, 不返回东西
             }
           }
 
-        } else if (dataEvents.contains(evName) &&
-                    !lastState.contains(evName)
+        } else if (dataEvents.contains(topicEvent) &&
+                  !lastEvents.contains(topicEvent)
         ) {
 
-          initState(data, evName, state) // 新建 state, 需要更新 state
+          initState(data, topicEvent, state) // 新建 state, 需要更新 state
 
-        } else if (!dataEvents.contains(evName) &&
-                     lastState.contains(evName)
+        } else if (!dataEvents.contains(topicEvent) && // 该事件这次没有出现
+                    lastEvents.contains(topicEvent)    // 该事件之前已经出现
         ) {
-
-          // 更新事件
-          lastState.map { ev =>
-            val updateState = getUpdateState(data, ev._1, lastState)
-            state.update(updateState)
+          // 更新事件的状态, 比如事件结束时间
+          lastEvents.map { ev =>
+            val eventName   = ev._1
+            val updatedState = getUpdatedEvent(data, eventName, lastEvents)
+            state.update(updatedState)
             None  // 在内存中更新事件, 不返回东西
           }
-
-          //None // 原始数据中没有, 但是 state 中有， 那什么也不做
         } else {
           None
         }
       }
 
-      if (lastState.size > 0) {
-        Some(vin, lastState)
+      if (lastEvents.size > 0) {
+        Some(vin, lastEvents)
       } else {
         None
       }
+
     })
   }
 
   /**
     * 事件正在进行, 则更新内存中的 state 对象, 不划分
-    * @param event 数据源
+    * @param data 数据源
     * @param evName 事件名
     * @param lastState 上一个 event 的状态
     * @return
     */
-  def getUpdateState(data: Event, evName: String, lastState: mutable.HashMap[String,EventUpdate]): mutable.HashMap[String,EventUpdate] = {
+  def getUpdatedEvent(data: Event, eventName: String, lastEvents: mutable.HashMap[String,EventUpdate]): mutable.HashMap[String,EventUpdate] = {
 
     val ts: Long = data.ts
-
-    val eventMaps = data.eventMaps
-    if (eventMaps.contains(evName)) {     // 如果有需要更新的事件, 则更新
-      lastState(evName).eventEndTime = ts // 事件正在进行, 将上一个事件的事件结束时间置为新的数据源的 ts
-      lastState(evName).eventStatus  = 1  // 事件正在进行, 这这种类型的事件状态置为 1
-    }
-
-    lastState
+    lastEvents(eventName).eventEndTime = ts // 事件正在进行, 将上一个事件的事件结束时间置为新的数据源的 ts
+    lastEvents(eventName).eventStatus  = 1  // 事件正在进行, 这这种类型的事件状态置为 1
+    lastEvents
   }
 
   /**
@@ -154,32 +156,32 @@ object EventsCheckImpl extends EventsCheck with Logging {
     * @param state HashMap[evName, EventUpdate]
     * @return HashMap[evName, EventUpdate]
     */
-  def getLastState(data: Event, state: State[mutable.HashMap[String, EventUpdate]]): mutable.HashMap[String,EventUpdate] = {
+  def getLastEvent(data: Event, state: State[mutable.HashMap[String, EventUpdate]]): mutable.HashMap[String,EventUpdate] = {
 
     if (state.exists()) {
       val dataEvents = data.eventMaps // 获取 data 中的 events
       val lastState = state.get()
 
       dataEvents.map { ev =>
-        val evName = ev._1
-        if (lastState.contains(evName)) { // state 里面包含已经出现的事件, 什么也不做，最后原样取回
+        val eventName = ev._1
+        if (lastState.contains(eventName)) { // state 里面包含已经出现的事件, 什么也不做，最后原样取回
             None  // 什么也不做, 没有副作用
         } else {  // state 中不包含该事件名, 要给该事件一个初始化状态， 就是在 lastState 里面加上一个新的键，而已
-          val newEvent = initEventUpdate(evName, data)
-          lastState.put(evName, newEvent)
+          val newEvent = initEvent(data, eventName)
+          lastState.put(eventName, newEvent)
         }
       }
 
-      lastState
+      lastState.clone()
 
     } else { // 如果 state 不存在, 说明这一批都是新数据, 则使用 event 数据源创建一个新的 eventUpdate 并返回这个对象，不操作内存中的 state
       val dataEvents: mutable.HashMap[String, Integer] = data.eventMaps // 获取 event 数据源 Map 中的事件
       val eventUpdateMaps: mutable.HashMap[String, EventUpdate] = scala.collection.mutable.HashMap() // 存放输出事件的 Map
 
       dataEvents.map { ev =>
-        val evName = ev._1
-        val newEventUpdate = initEventUpdate(evName, data)
-        eventUpdateMaps.put(evName, newEventUpdate)
+        val eventName = ev._1
+        val newEvent = initEvent(data, eventName)
+        eventUpdateMaps.put(eventName, newEvent)
       }
 
       eventUpdateMaps
@@ -188,30 +190,30 @@ object EventsCheckImpl extends EventsCheck with Logging {
 
   /**
     * 初始化一个新的事件
-    * @param event 数据源
+    * @param data 数据源
     * @param evName 事件名
     * @param state HashMap[eventName, EventUpdate]
     */
-  def initState(data: Event, evName: String, state: State[mutable.HashMap[String, EventUpdate]]) = {
+  def initState(data: Event, eventName: String, state: State[mutable.HashMap[String, EventUpdate]]) = {
 
-    val newEvent = initEventUpdate(evName, data)
+    val newEvent = initEvent(data, eventName)
     val lastState: mutable.HashMap[String, EventUpdate] = state.get()
-    lastState.put(evName, newEvent)
+    lastState.put(eventName, newEvent)
     state.update(lastState)
   }
 
   // 初始化单个事件的输出对象
-  def initEventUpdate(evName: String, ev: Event): EventUpdate = {
-    val vin            = ev.vin
-    val eventStartTime = ev.ts
-    val eventEndTime   = ev.ts
+  def initEvent(data: Event, eventName: String): EventUpdate = {
+    val vin            = data.vin
+    val eventStartTime = data.ts
+    val eventEndTime   = data.ts
     val eventStatus    = 1
     val eventType      = 1
-    val startMileage   = ev.veh_odo
+    val startMileage   = data.veh_odo
 
     EventUpdate(
       vin,
-      evName,
+      eventName,
       eventStartTime,
       eventEndTime,
       eventStatus,
